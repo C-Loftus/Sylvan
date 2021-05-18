69f68c671878613492f8aa1b9d5926e421ed7262
colton <cloftus@princeton.edu>
2021-05-17 23:59:56 -0400

    Improved images and CSS

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ from flask import Flask, jsonify, render_template, url_for, request, redirect, s
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from authlib.integrations.flask_client import OAuth
-
+from werkzeug.utils import secure_filename
 import os, json
 
 # App Initialization
 app = Flask(__name__)
+app.config["IMAGE_UPLOADS"] = "/home/colton/Projects/PlantApp/static/Images"
+app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["JPEG", "JPG", "PNG", "GIF"]
+app.config["MAX_IMAGE_FILESIZE"] = 0.5 * 1024 * 1024
 oauth = OAuth(app)
 app.secret_key = 'random secret'
 
@@ -72,11 +75,35 @@ def index():
 def newPost():
     if request.method == 'POST':
         email = dict(session).get('email', None)
-        image_link =      request.form.get('imageLink', False)
+        # image = request.files("image")
+        # if image.filename == "":
+        #     print("No filename")
+        #     return redirect(request.url)
+        # if allowed_image(image.filename):
+        #     filename = secure_filename(image.filename)
+        #     image.save(os.path.join(app.config["IMAGE_UPLOADS"], filename))    
+        #     image_link = filename
+        image_link = ""
+        if request.files:
+            print("Is a file")
+            # if not allowed_image_filesize(request.cookies["filesize"]):
+            #     print("Filesize exceeded maximum limit")
+            #     return redirect(request.url)
+            image = request.files["image"]
+            if image.filename == "":
+                print("No filename")
+            if allowed_image(image.filename):
+                filename = secure_filename(image.filename)
+                image.save(os.path.join(app.config["IMAGE_UPLOADS"], filename))
+                print(image)
+                image_link = filename
+            else:
+                print("That file extension is not allowed")
         plant_Name =      request.form.get('plantName', False)
         post_title =      request.form.get('title', False)
         post_content =    request.form.get('description', False)
         post_location =   request.form.get('location', False)
+
         if email is str:
             post_author = email
         else:
@@ -121,18 +148,24 @@ def edit(id):
     else:
         return render_template('edit.html', post=post)
 
-app.config["IMAGE_UPLOADS"] = "~/Projects/PlantApp"
-@app.route("/newpost", methods=["GET", "POST"])
-def upload_image():
-    if request.method == "POST":
-        if request.files:
-            image = request.files["image"]
-            print(image)
-            image.save(os.path.join(app.config["IMAGE_UPLOADS"], image.filename))
-            print("Image saved")
-            return redirect(request.url)
-    return render_template("index.html")
+def allowed_image(filename):
+    # We only want files with a . in the filename
+    if not "." in filename:
+        return False
+    # Split the extension from the filename
+    ext = filename.rsplit(".", 1)[1]
+    # Check if the extension is in ALLOWED_IMAGE_EXTENSIONS
+    if ext.upper() in app.config["ALLOWED_IMAGE_EXTENSIONS"]:
+        return True
+    else:
+        return False
 
+# def allowed_image_filesize(filesize):
+    
+#     if int(filesize) <= app.config["MAX_IMAGE_FILESIZE"]:
+#         return True
+#     else:
+#         return False
 
 # Run in debug mode if not deployed
 if __name__ == "__main__":
